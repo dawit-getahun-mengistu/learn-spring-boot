154e29005930eec079ee5ff6de9bb726f013922c
Dawit Getahun Mengistu <dawitgetahunmengistu@gmail.com>
2024-08-21 12:09:57 +0300

    add controller and service layers with dependency injection

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,34 +1,15 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello(){
-		return List.of(
-				new Student(
-						1L,
-						"Dawit",
-						LocalDate.of(2002, Month.APRIL, 20),
-						21,
-						"dawit@gmail.com"
 
-				)
-		);
-	}
 }
